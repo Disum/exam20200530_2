cf12b984e16fe3928018d183a1754bb3598444c3
yakov <mirenkovye@yandex.ru>
2020-05-30 13:52:28 +0300

    Finish new task

--- matrix.c
+++ matrix.c
@@ -8,7 +8,7 @@
 int read_matrix(char *name, int *spaces, char **a, int m, int n)
 {
 	FILE *input;
-	int i, j, k, l;
+	int i, j, k, l, end_string;
 	char buff[MAX_LEN];
 
 	if( !(input = fopen(name, "r")) )
@@ -16,12 +16,17 @@ int read_matrix(char *name, int *spaces, char **a, int m, int n)
 
 	for( i = 0; i<m; i++ )
 	{
+		end_string = 0;
 		for( j = 0; j<n; j++ )
 		{
-			for( k = 0; k<MAX_LEN; k++ )
+			k = 0;
+			if( !end_string )
 			{
-				if( fscanf(input, "%c", buff + k)!=1 || spaces[(int)buff[k]] )
-					break;
+				for( ; k<MAX_LEN; k++ )
+				{
+					if( fscanf(input, "%c", buff + k)!=1 || spaces[(int)buff[k]] )
+						break;
+				}
 			}
 
 			if( !(a[i*n + j] = (char *)malloc((k + 1)*sizeof(char))) )
@@ -32,6 +37,9 @@ int read_matrix(char *name, int *spaces, char **a, int m, int n)
 			for( l = 0; l<k; l++ )
 				a[i*n + j][l] = buff[l];
 			a[i*n + j][k] = '\0';
+
+			if( buff[k]=='\n' || buff[k]=='\0' )
+				end_string = 1;
 		}
 	}
 
